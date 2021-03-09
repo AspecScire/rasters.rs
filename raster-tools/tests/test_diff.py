9ad685e899b8a46dca268e83114064a0390e0294
Rajsekar Manokaran <rajsekar@gmail.com>
2021-03-09 18:22:57 +0530

    Improve tests for tools
    + set chunk size to 1 to test chunking
    + make diff sample rasters rectangular
    + update tile fixture for fix to max-zoom

--- raster-tools/tests/test_diff.py
+++ raster-tools/tests/test_diff.py
@@ -9,15 +9,15 @@ with TemporaryDirectory(prefix="test-raster-diff-") as base_path:
     base_path = Path(base_path)
 
     raster1_path = base_path / "raster1.tif"
-    data1 = N.random.normal(size=(64, 64, 1))
-    create_raster(raster1_path, data1).SetGeoTransform([-32., 1., 0., -32., 0., 1.])
+    data1 = N.random.normal(size=(64, 128, 1))
+    create_raster(raster1_path, data1).SetGeoTransform([-64., 1., 0., -32., 0., 1.])
 
     raster2_path = base_path / "raster2.tif"
-    data2 = N.random.normal(size=(32, 32, 1))
-    create_raster(raster2_path, data2).SetGeoTransform([-64., 4., 0., -64., 0., 4.])
+    data2 = N.random.normal(size=(32, 64, 1))
+    create_raster(raster2_path, data2).SetGeoTransform([-128., 4., 0., -64., 0., 4.])
 
     stats = run_cargo('raster-diff', str(raster1_path), str(raster2_path))['stats']['diff']
-    diff = data2[8:-8, 8:-8, 0].repeat(4, axis=0).repeat(4, axis=1) - data1[:,:,0]
+    diff = data2[8:-8, 16:-16, 0].repeat(4, axis=0).repeat(4, axis=1) - data1[:,:,0]
 
     assert_is_close(stats['max'], N.max(diff), desc='max')
     assert_is_close(stats['min'], N.min(diff), desc='min')
